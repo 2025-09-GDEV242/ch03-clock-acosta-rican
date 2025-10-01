2f8d3a3bbf77b48b7e036e9211b59be6977aabb8
acosta-rican <111157825+acosta-rican@users.noreply.github.com>
2025-09-30 22:11:48 -0400

    Final commit to 24hrInternal branch.
    Edited to satisfy book exercise 3.39.
    It's important to note the time of this commit submission is 09/30/25 @10:11 PM.

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -2,6 +2,11 @@
 /**
  * 24 hr Internal Clock.
  * 
+ * Edited original source code to satisfy book exercise 3.39.
+ * 
+ * Out of 3.38, and 3.39, this was the easiest to deal program due to most internal values staying stagnant.
+ * Where as in 3.38, there was more abstraction involved revolving '0' and '12'.
+ * 
  * The ClockDisplay class implements a digital clock display for a
  * European-style 24 hour clock. The clock shows hours and minutes. The 
  * range of the clock is 00:00 (midnight) to 23:59 (one minute before 
@@ -20,12 +25,13 @@ public class ClockDisplay
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
+    private Boolean amPM = true; // new field to decide AM/PM. 'amPM = true' means it's in the AM, 'false' means PM.
     private String displayString;    // simulates the actual display
     
-    /**
-     * Constructor for ClockDisplay objects. This constructor 
-     * creates a new clock set at 00:00.
-     */
+        /**
+         * Constructor for ClockDisplay objects. This constructor 
+         * creates a new clock set at 00:00.
+         */
     public ClockDisplay()
     {
         hours = new NumberDisplay(24);
@@ -54,6 +60,12 @@ public class ClockDisplay
         minutes.increment();
         if(minutes.getValue() == 0) {  // it just rolled over!
             hours.increment();
+            if ( ((hours.getValue() / 12 ) % 2) == 0 ){
+            amPM = true;
+            }
+            else {
+            amPM = false; 
+            }
         }
         updateDisplay();
     }
@@ -64,7 +76,13 @@ public class ClockDisplay
      */
     public void setTime(int hour, int minute)
     {
-        hours.setValue(hour);
+        if ( ((hour / 12 ) % 2) == 0 ){
+            amPM = true;
+        }
+        else {
+            amPM = false; 
+        }
+        hours.setValue(hour%12);
         minutes.setValue(minute);
         updateDisplay();
     }
@@ -82,7 +100,11 @@ public class ClockDisplay
      */
     private void updateDisplay()
     {
-        displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+        if ( amPM == true ){
+           displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + " AM.";
+        }
+        else {
+            displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + " PM.";
+        }
     }
-}
+}//end of code.
